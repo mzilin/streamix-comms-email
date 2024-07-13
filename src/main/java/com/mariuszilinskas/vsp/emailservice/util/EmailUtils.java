315a62328d695a904351c72f6226b7f390390ff0
Marius Zilinskas <mariuzmail@gmail.com>
2024-07-13 12:29:11 +0100

    setup email utils

---
+++ src/main/java/com/mariuszilinskas/vsp/emailservice/util/EmailUtils.java
@@ -0,0 +1,11 @@
+package com.mariuszilinskas.vsp.emailservice.util;
+
+public abstract class EmailUtils {
+
+    private EmailUtils() {
+        // Private constructor to prevent instantiation
+    }
+
+    public static final String TIMESTAMP_FORMAT = "yyyy-MM-dd hh:mm:ss";
+
+}

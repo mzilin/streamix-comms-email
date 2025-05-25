d903681beb9c8e3a3eb030d06a7a495b9b247726
Marius Zilinskas <mariuzmail@gmail.com>
2025-05-25 17:13:51 +0100

    add placeholders to application.yml

--- src/test/java/com/mariuszilinskas/vsp/comms/email/EmailServiceApplicationTests.java
+++ src/test/java/com/mariuszilinskas/vsp/comms/email/EmailServiceApplicationTests.java
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 /**
  * Test class for the Spring application context and bean configuration in the EmailService application.
  */
-@SpringBootTest(properties = "eureka.client.enabled=false")
+@SpringBootTest
 class EmailServiceApplicationTests {
 
 	@Autowired
